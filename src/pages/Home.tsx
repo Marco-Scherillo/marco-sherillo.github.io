40759daf6c2ec5654df61ff9bc68f43edc4ad48c
root <root@DESKTOP-H0G33IQ.localdomain>
2026-01-20 16:02:30 -0500

    fixed issue with linked in link

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -82,7 +82,7 @@ export default function Home() {
             </a>
 
             <a
-              href="www.linkedin.com/in/marco-scherillo"
+              href="https://www.linkedin.com/in/marco-scherillo"
               target="_blank"
               rel="noopener noreferrer"
               aria-label="LinkedIn"
